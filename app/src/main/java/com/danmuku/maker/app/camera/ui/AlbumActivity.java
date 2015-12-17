14bcbb6423caa1c8f31be6773f1dde705ecaa3f0
veryyoung <codingyoung@gmail.com>
2015-12-17 19:44:13 +0800

    init the code

---
+++ app/src/main/java/com/danmuku/maker/app/camera/ui/AlbumActivity.java
@@ -0,0 +1,97 @@
+package com.danmuku.maker.app.camera.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.danmuku.maker.R;
+import com.danmuku.maker.util.FileUtils;
+import com.danmuku.maker.util.ImageUtils;
+import com.danmuku.maker.util.StringUtils;
+import com.danmuku.maker.customview.PagerSlidingTabStrip;
+import com.danmuku.maker.AppConstants;
+import com.danmuku.maker.app.camera.CameraBaseActivity;
+import com.danmuku.maker.app.camera.fragment.AlbumFragment;
+import com.danmuku.maker.app.model.Album;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
+/**
+ * 相册界面
+ */
+public class AlbumActivity extends CameraBaseActivity {
+
+    private Map<String, Album> albums;
+    private List<String> paths = new ArrayList<String>();
+
+    @InjectView(R.id.indicator)
+    PagerSlidingTabStrip tab;
+    @InjectView(R.id.pager)
+    ViewPager pager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_album);
+        ButterKnife.inject(this);
+        albums = ImageUtils.findGalleries(this, paths, 0);
+        //ViewPager的adapter
+        FragmentPagerAdapter adapter = new TabPageIndicatorAdapter(getSupportFragmentManager());
+        pager.setAdapter(adapter);
+        tab.setViewPager(pager);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent result) {
+        if (requestCode == AppConstants.REQUEST_CROP && resultCode == RESULT_OK) {
+            Intent newIntent = new Intent(this, PhotoProcessActivity.class);
+            newIntent.setData(result.getData());
+            startActivity(newIntent);
+        }
+    }
+
+
+    class TabPageIndicatorAdapter extends FragmentPagerAdapter {
+        public TabPageIndicatorAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            //新建一个Fragment来展示ViewPager item的内容，并传递参数
+            return AlbumFragment.newInstance(albums.get(paths.get(position)).getPhotos());
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Album album = albums.get(paths.get(position % paths.size()));
+            if (StringUtils.equalsIgnoreCase(FileUtils.getInst().getSystemPhotoPath(),
+                    album.getAlbumUri())) {
+                return "胶卷相册";
+            } else if (album.getTitle().length() > 13) {
+                return album.getTitle().substring(0, 11) + "...";
+            }
+            return album.getTitle();
+        }
+
+        @Override
+        public int getCount() {
+            return paths.size();
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/danmuku/maker/app/camera/ui/PhotoProcessActivity.java
@@ -0,0 +1,295 @@
+package com.danmuku.maker.app.camera.ui;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.RectF;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.danmuku.maker.R;
+import com.danmuku.maker.util.FileUtils;
+import com.danmuku.maker.util.ImageUtils;
+import com.danmuku.maker.util.StringUtils;
+import com.danmuku.maker.util.TimeUtils;
+import com.danmuku.maker.customview.LabelView;
+import com.danmuku.maker.customview.MyHighlightView;
+import com.danmuku.maker.customview.MyImageViewDrawableOverlay;
+import com.danmuku.maker.App;
+import com.danmuku.maker.AppConstants;
+import com.danmuku.maker.app.camera.CameraBaseActivity;
+import com.danmuku.maker.app.camera.CameraManager;
+import com.danmuku.maker.app.camera.util.EffectUtil;
+import com.danmuku.maker.app.model.FeedItem;
+import com.danmuku.maker.app.model.TagItem;
+import com.danmuku.maker.app.ui.EditTextActivity;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import de.greenrobot.event.EventBus;
+import it.sephiroth.android.library.widget.HListView;
+
+/**
+ * 图片处理界面
+ */
+public class PhotoProcessActivity extends CameraBaseActivity {
+
+    //滤镜图片
+    @InjectView(R.id.draw_image)
+    ImageView imageView;
+
+    //绘图区域
+    @InjectView(R.id.drawing_view_container)
+    ViewGroup drawArea;
+
+    //工具区
+    @InjectView(R.id.list_tools)
+    HListView bottomToolBar;
+
+    @InjectView(R.id.toolbar_area)
+    ViewGroup toolArea;
+
+    private MyImageViewDrawableOverlay mImageView;
+
+    //当前图片
+    private Bitmap currentBitmap;
+
+    //用于预览的小图片
+    private Bitmap smallImageBackgroud;
+    //小白点标签
+    private LabelView emptyLabelView;
+
+    private List<LabelView> labels = new ArrayList<LabelView>();
+
+    //标签区域
+    private View commonLabelArea;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_process);
+        ButterKnife.inject(this);
+        EffectUtil.clear();
+        initView();
+        initEvent();
+
+        ImageUtils.asyncLoadImage(this, getIntent().getData(), result -> {
+            currentBitmap = result;
+            imageView.setImageBitmap(currentBitmap);
+        });
+
+        ImageUtils.asyncLoadSmallImage(this, getIntent().getData(), result -> smallImageBackgroud = result);
+
+    }
+
+    private void initView() {
+        //添加贴纸水印的画布
+        View overlay = LayoutInflater.from(PhotoProcessActivity.this).inflate(
+                R.layout.view_drawable_overlay, null);
+        mImageView = (MyImageViewDrawableOverlay) overlay.findViewById(R.id.drawable_overlay);
+        ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(App.getApp().getScreenWidth(),
+                App.getApp().getScreenWidth());
+        mImageView.setLayoutParams(params);
+        overlay.setLayoutParams(params);
+        drawArea.addView(overlay);
+
+        //添加标签选择器
+        RelativeLayout.LayoutParams rparams = new RelativeLayout.LayoutParams(App.getApp().getScreenWidth(), App.getApp().getScreenWidth());
+
+        //初始化滤镜图片
+        imageView.setLayoutParams(rparams);
+
+
+        //初始化空白标签
+        emptyLabelView = new LabelView(this);
+        emptyLabelView.setEmpty();
+        EffectUtil.addLabelEditable(mImageView, drawArea, emptyLabelView,
+                mImageView.getWidth() / 2, mImageView.getWidth() / 2);
+        emptyLabelView.setVisibility(View.INVISIBLE);
+
+        //初始化推荐标签栏
+        commonLabelArea = LayoutInflater.from(PhotoProcessActivity.this).inflate(
+                R.layout.view_label_bottom, null);
+        commonLabelArea.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        toolArea.addView(commonLabelArea);
+        commonLabelArea.setVisibility(View.GONE);
+    }
+
+    private void initEvent() {
+
+
+        bottomToolBar.setVisibility(View.GONE);
+        commonLabelArea.setVisibility(View.VISIBLE);
+
+
+        mImageView.setOnDrawableEventListener(wpEditListener);
+        mImageView.setSingleTapListener(() -> {
+            emptyLabelView.updateLocation((int) mImageView.getmLastMotionScrollX(),
+                    (int) mImageView.getmLastMotionScrollY());
+            emptyLabelView.setVisibility(View.VISIBLE);
+
+            EditTextActivity.openTextEdit(PhotoProcessActivity.this, "", 8, AppConstants.ACTION_EDIT_LABEL);
+
+            drawArea.postInvalidate();
+        });
+
+
+        titleBar.setRightBtnOnclickListener(v -> {
+            savePicture();
+        });
+    }
+
+    //保存图片
+    private void savePicture() {
+        //加滤镜
+        final Bitmap newBitmap = Bitmap.createBitmap(mImageView.getWidth(), mImageView.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas cv = new Canvas(newBitmap);
+        RectF dst = new RectF(0, 0, mImageView.getWidth(), mImageView.getHeight());
+
+        cv.drawBitmap(currentBitmap, null, dst, null);
+        //加贴纸水印
+        EffectUtil.applyOnSave(cv, mImageView);
+
+        new SavePicToFileTask().execute(newBitmap);
+    }
+
+    private class SavePicToFileTask extends AsyncTask<Bitmap, Void, String> {
+        Bitmap bitmap;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            showProgressDialog("图片处理中...");
+        }
+
+        @Override
+        protected String doInBackground(Bitmap... params) {
+            String fileName = null;
+            try {
+                bitmap = params[0];
+
+                String picName = TimeUtils.dtFormat(new Date(), "yyyyMMddHHmmss");
+                fileName = ImageUtils.saveToFile(FileUtils.getInst().getPhotoSavedPath() + "/" + picName, false, bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                toast("图片处理错误，请退出相机并重试", Toast.LENGTH_LONG);
+            }
+            return fileName;
+        }
+
+        @Override
+        protected void onPostExecute(String fileName) {
+            super.onPostExecute(fileName);
+            dismissProgressDialog();
+            if (StringUtils.isEmpty(fileName)) {
+                return;
+            }
+
+            //将照片信息保存至sharedPreference
+            //保存标签信息
+            List<TagItem> tagInfoList = new ArrayList<TagItem>();
+            for (LabelView label : labels) {
+                tagInfoList.add(label.getTagInfo());
+            }
+
+            //将图片信息通过EventBus发送到MainActivity
+            FeedItem feedItem = new FeedItem(tagInfoList, fileName);
+            EventBus.getDefault().post(feedItem);
+            CameraManager.getInst().close();
+        }
+    }
+
+
+    public void tagClick(View v) {
+        TextView textView = (TextView) v;
+        TagItem tagItem = new TagItem(AppConstants.POST_TYPE_TAG, textView.getText().toString());
+        addLabel(tagItem);
+    }
+
+    private MyImageViewDrawableOverlay.OnDrawableEventListener wpEditListener = new MyImageViewDrawableOverlay.OnDrawableEventListener() {
+        @Override
+        public void onMove(MyHighlightView view) {
+        }
+
+        @Override
+        public void onFocusChange(MyHighlightView newFocus, MyHighlightView oldFocus) {
+        }
+
+        @Override
+        public void onDown(MyHighlightView view) {
+
+        }
+
+        @Override
+        public void onClick(MyHighlightView view) {
+//            labelSelector.hide();
+        }
+
+        @Override
+        public void onClick(final LabelView label) {
+            if (label.equals(emptyLabelView)) {
+                return;
+            }
+            alert("温馨提示", "是否需要删除该标签！", "确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    EffectUtil.removeLabelEditable(mImageView, drawArea, label);
+                    labels.remove(label);
+                }
+            }, "取消", null);
+        }
+    };
+
+
+    //添加标签
+    private void addLabel(TagItem tagItem) {
+        emptyLabelView.setVisibility(View.INVISIBLE);
+        if (labels.size() >= 5) {
+            alert("温馨提示", "您只能添加5个标签！", "确定", null, null, null, true);
+        } else {
+            int left = emptyLabelView.getLeft();
+            int top = emptyLabelView.getTop();
+            if (labels.size() == 0 && left == 0 && top == 0) {
+                left = mImageView.getWidth() / 2 - 10;
+                top = mImageView.getWidth() / 2;
+            }
+            LabelView label = new LabelView(PhotoProcessActivity.this);
+            label.init(tagItem);
+            EffectUtil.addLabelEditable(mImageView, drawArea, label, left, top);
+            labels.add(label);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (AppConstants.ACTION_EDIT_LABEL == requestCode && data != null) {
+            String text = data.getStringExtra(AppConstants.PARAM_EDIT_TEXT);
+            if (StringUtils.isNotEmpty(text)) {
+                TagItem tagItem = new TagItem(AppConstants.POST_TYPE_TAG, text);
+                addLabel(tagItem);
+            }
+        } else if (AppConstants.ACTION_EDIT_LABEL_POI == requestCode && data != null) {
+            String text = data.getStringExtra(AppConstants.PARAM_EDIT_TEXT);
+            if (StringUtils.isNotEmpty(text)) {
+                TagItem tagItem = new TagItem(AppConstants.POST_TYPE_POI, text);
+                addLabel(tagItem);
+            }
+        }
+    }
+}
